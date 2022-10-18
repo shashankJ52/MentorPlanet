b7d4a21ecb9404d62570e7cddb54ba1ccb56de5f
Shashank Jajimoggala <66770133+shashankJ52@users.noreply.github.com>
2022-10-18 13:27:20 +0530

    Basic Structure Complete

--- main.js
+++ main.js
@@ -12,4 +12,18 @@ window.onload = function load_func() {
         collapse.classList.toggle('active')
         // console.log(toggle)
     });
+
+
+    //mansonry JS
+    let grid = document.querySelector("#site-main .recent-work-area .images-flex")
+    let msnry = new Masonry(grid, {
+        itemSelector: '.flex-items',
+        gutter: 100,
+        fitWidth:true
+    })
 }
+
+// Rellax Js
+var rellax = new Rellax('.rellax', {
+    center: true
+})
